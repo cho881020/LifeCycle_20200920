82cef72cca2e7ff058358f65ab05dc4ba6cf4c45
조경진 <kj_cho@idealidea.co.kr>
2020-09-20 15:35:57 +0900

    onCreate 실험

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
@@ -1,11 +1,21 @@
 package kr.co.tjoeun.lifecycle_20200920
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("메인화면", "onCreate 실행됨")
+
+        moveToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
